127a252bab44b13d49b915846bc7389f8f1ff795
KarolisSad <92150126+KarolisSad@users.noreply.github.com>
2021-12-13 01:49:37 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import view.MyApplication;
+
+public class Main
+{
+    public static void main(String[] args)
+  {
+    Application.launch(MyApplication.class);
+  }
+}
